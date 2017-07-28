1d9f639de42342da0166db47102d903ac49872f6
zhengzhongwang <zhengzhongwang@sohu-inc.com>
2017-07-28 17:34:51 +0800

    init

---
+++ matrix-demo/src/main/java/com/solid4j/matrix/demo/controller/UserController.java
@@ -0,0 +1,12 @@
+/*
+ * Copyright (c) 2017 Sohu. All Rights Reserved
+ */
+package com.solid4j.matrix.demo.controller;
+
+/**
+ * @author: solidwang
+ * @since 1.0
+ */
+public class UserController {
+
+}
